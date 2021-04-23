b952d5fe4c1c859b42e9951ab3df004056e5e427
Eden\mohang <1032947610@qq.ocm>
2021-04-23 17:21:07 +0800

    demo层分离

--- flutter_app/lib/demo/commodity_list_demo.dart
+++ flutter_app/lib/demo/commodity_list_demo.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-import '../http/commodity_list_http.dart';
-import 'commodity_details_normal_demo.dart';
+import 'commodity_list_normal.dart';
 
+//商品列表页面主页面
 class CommodityListDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,69 +13,6 @@ class CommodityListDemo extends StatelessWidget {
         elevation: 0.0,
       ),
       body: CommodityListHome(),
-    );
-  }
-}
-
-class CommodityListHome extends StatefulWidget {
-  @override
-  _CommodityListHomeState createState() => _CommodityListHomeState();
-}
-
-class _CommodityListHomeState extends State<CommodityListHome> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Future<Null> _onRefresh() async {
-      await Future.delayed(Duration(seconds: 3), () {
-        print('refresh');
-        setState(() {
-          fetchCommodityLists();
-        });
-      });
-    }
-
-    return RefreshIndicator(
-      onRefresh: _onRefresh,
-      child: FutureBuilder(
-        future: fetchCommodityLists(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          print('data: ${snapshot.data}');
-          print('connectionState: ${snapshot.connectionState}');
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: Text('loading...'),
-            );
-          }
-          return ListView(
-            children: snapshot.data.map<Widget>((item) {
-              return ListTile(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context) => DtailsNormalList(
-                            pageTitle: '商品详情',
-                          )));
-                },
-                title: Text(
-                  "商品名:${item.title}",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-                ),
-                subtitle: Text("商品id:${item.id}\n价格:${item.price}"),
-                contentPadding: EdgeInsets.symmetric(vertical: 10.0),
-                leading: Image.network(
-                  item.imageUrl,
-                  height: 200.0,
-                  width: 130.0,
-                ),
-              );
-            }).toList(),
-          );
-        },
-      ),
     );
   }
 }
